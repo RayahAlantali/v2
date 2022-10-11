5e0dcd196f85c0421fadc259f9f51e0131967771
CharlieMonash <cdix0004@student.monash.edu>
2022-10-11 22:40:14 +1100

    NO RRTC

--- slam/ekf.py
+++ slam/ekf.py
@@ -139,15 +139,13 @@ class EKF:
 
         # TODO: add your codes here to compute the updated x
         #Compute Kalman Gain
-        S = H @ self.P @ H.T + R #+ 0.01*np.eye(R.shape[0])
+        S = H @ self.P @ H.T + R 
         K = self.P @ H.T @ np.linalg.inv(S)
-
-        #Correct state
+        #Adjusting the state
         y = z - z_hat
         x = x + K @ y
+        #Setting state estimate and covariance
         self.set_state_vector(x)
-
-        #Correct covariance
         self.P = (np.eye(x.shape[0]) - K @ H) @ self.P
 
     def state_transition(self, raw_drive_meas):
