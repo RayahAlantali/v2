bdd85681a757e14119f6e6575d7e47b713262e78
CharlieMonash <cdix0004@student.monash.edu>
2022-10-11 19:06:50 +1100

    rrt

--- path_planning/RRT2.py
+++ path_planning/RRT2.py
@@ -1,11 +1,14 @@
-class RRTC:
+
+class RRT:
     """
     Class for RRT planning
     """
+
     class Node:
         """
         RRT Node
         """
+
         def __init__(self, x, y):
             self.x = x
             self.y = y
@@ -38,60 +41,47 @@ class RRTC:
         self.path_resolution = path_resolution
         self.max_nodes = max_points
         self.obstacle_list = obstacle_list
-        self.start_node_list = [] # Tree from start
-        self.end_node_list = [] # Tree from end
+        self.node_list = []
 
-    def planning(self):
+    def planning(self, animation=True):
         """
         rrt path planning
+        animation: flag for animation on or off
         """
-        self.start_node_list = [self.start]
-        self.end_node_list = [self.end]
-        while len(self.start_node_list) + len(self.end_node_list) <= self.max_nodes:
 
-        #TODO: Complete the planning method ----------------------------------------------------------------
-            # 1. Sample and add a node in the start tree
+        self.node_list = [self.start]
+        while len(self.node_list) <= self.max_nodes:
+
+            # 1. Generate a random node
             rnd_node = self.get_random_node()
-            expansion_ind = self.get_nearest_node_index(self.start_node_list, rnd_node)
-            expansion_node = self.start_node_list[expansion_ind]
-            new_node = self.steer(expansion_node, rnd_node, self.expand_dis)
-            if self.is_collision_free(new_node):
-                self.start_node_list.append(new_node)
 
+            # 2. Find node in tree that is closest to sampled node.
+            # This is the node to be expanded (q_expansion)
+            expansion_ind = self.get_nearest_node_index(self.node_list, rnd_node)
+            expansion_node = self.node_list[expansion_ind]
 
-            # 2. Check whether trees can be connected
-            node_index = self.get_nearest_node_index(self.end_node_list, new_node)
-            node = self.end_node_list[node_index]
+            #TODO:  Complete the last two main steps of the RRT algorithm ----------------
+            # 3. Select a node (nearby_node) close to expansion_node by moving from expantion_node to rnd_node
+            # Use the steer method
+            nearby_node = self.steer(expansion_node, rnd_node, self.expand_dis)
+            # 4. Check if nearby_node is in free space (i.e., it is collision free). If collision free, add node
+            # to self.node_list
+            if self.is_collision_free(nearby_node):
+                self.node_list.append(nearby_node)
 
-            distance, _ = self.calc_distance_and_angle(node, new_node)
-            if distance <= self.expand_dis:
+            # Please remove return None when you start coding
+            # return None
+            #ENDTODO -----------------------------------------------------------------------
 
-
-            # 3. Add the node that connects the trees and generate the path
-                # Note: It is important that you return path found as:
-                # return self.generate_final_course(len(self.start_node_list) - 1, len(self.end_node_list) - 1)
-                if self.is_collision_free(new_node):
-                    self.end_node_list.append(new_node)
-                    self.start_node_list.append(node)
-                    return self.generate_final_course(len(self.start_node_list) - 1, len(self.end_node_list) - 1)
-            # 4. Sample and add a node in the end tree
-
-            else:
-                rnd_node = self.get_random_node()
-                expansion_ind = self.get_nearest_node_index(self.end_node_list, rnd_node)
-                expansion_node = self.end_node_list[expansion_ind]
-                new_node = self.steer(expansion_node, rnd_node, self.expand_dis)
-                if self.is_collision_free(new_node):
-                    self.end_node_list.append(new_node)
-
-            # 5. Swap start and end trees
-            self.start_node_list, self.end_node_list = self.end_node_list, self.start_node_list
-
-        #ENDTODO ----------------------------------------------------------------------------------------------
+            # If we are close to goal, stop expansion and generate path
+            if self.calc_dist_to_goal(self.node_list[-1].x, self.node_list[-1].y) <= self.expand_dis:
+                final_node = self.steer(self.node_list[-1], self.end, self.expand_dis)
+                if self.is_collision_free(final_node):
+                    return self.generate_final_course(len(self.node_list) - 1)
 
         return None  # cannot find path
 
-    # ------------------------------DO NOT change helper methods below ----------------------------
+
     def steer(self, from_node, to_node, extend_length=float("inf")):
         """
         Given two nodes from_node, to_node, this method returns a node new_node such that new_node
@@ -127,6 +117,7 @@ class RRTC:
 
         return new_node
 
+
     def is_collision_free(self, new_node):
         """
         Determine if nearby_node (new_node) is in the collision-free space.
@@ -142,21 +133,13 @@ class RRTC:
 
         return True  # safe
 
-    def generate_final_course(self, start_mid_point, end_mid_point):
+
+    def generate_final_course(self, goal_ind):
         """
         Reconstruct path from start to end node
         """
-        # First half
-        node = self.start_node_list[start_mid_point]
-        path = []
-        while node.parent is not None:
-            path.append([node.x, node.y])
-            node = node.parent
-        path.append([node.x, node.y])
-
-        # Other half
-        node = self.end_node_list[end_mid_point]
-        path = path[::-1]
+        path = [[self.end.x, self.end.y]]
+        node = self.node_list[goal_ind]
         while node.parent is not None:
             path.append([node.x, node.y])
             node = node.parent
@@ -182,13 +165,13 @@ class RRTC:
         dlist = [(node.x - rnd_node.x) ** 2 + (node.y - rnd_node.y)
                  ** 2 for node in node_list]
         minind = dlist.index(min(dlist))
+
         return minind
 
     @staticmethod
     def calc_distance_and_angle(from_node, to_node):
         dx = to_node.x - from_node.x
         dy = to_node.y - from_node.y
-        d = math.hypot(dx, dy)
+        d = math.hypot(dx, dy) #returns the Euclidean norm
         theta = math.atan2(dy, dx)
         return d, theta
-
