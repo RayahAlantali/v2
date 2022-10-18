40a1e518a269f35d45844f430d1b665cd6fc733b
tkus0002 <tkus0002@student.monash.edu>
2022-10-19 10:33:57 +1100

    fuit

--- network/scripts/detector.py
+++ network/scripts/detector.py
@@ -50,7 +50,7 @@ class Detector:
             stored_result = np.array([predic_class,probability,xl,xu,yl,yu])
         #Save all the predictions to a file with the boxes
         image_data = pred.pandas().xyxy[0]
-        with open('fuit_estimates/targets.txt', 'a') as fo:
+        with open('fruit_estimates/targets.txt', 'a') as fo:
             json.dump(image_data, fo)
         return np.squeeze(pred.render()),np.squeeze(pred.render()),pred_results
 
